6fdc08fae9b222da89490423b012d363cee6ee4a
Nathan Sturtevant <nathanst@gmail.com>
2025-02-24 23:20:02 -0700

    Added ability to modify button fill color after creation

--- Driver.cpp
+++ Driver.cpp
@@ -6,8 +6,11 @@
 #include "Timer.h"
 #include "PermutationPDB.h"
 #include "TemplateAStar.h"
+#include "FileUtil.h"
+#include "SVGUtil.h"
+const int numDisks = 16;
 
-const int numDisks = 14;
+void SaveSVG(Graphics::Display &d, int port);
 
 TOH<numDisks> toh;
 TOHState<numDisks> s, g;
@@ -22,17 +25,21 @@ Heuristic<TOHState<numDisks>> *BuildPDB(const TOHState<numDisks> &goal);
 
 // PDB info
 TOHState<numDisks> goal;
-TOH<numDisks-2> absToh1;
-TOH<numDisks-3> absToh2;
+TOH<numDisks-6> absToh1;
+TOH<numDisks-12> absToh2;
 TOH<numDisks-4> absToh3;
-TOHState<numDisks-2> absTohState1;
-TOHState<numDisks-3> absTohState2;
+TOHState<numDisks-6> absTohState1;
+TOHState<numDisks-12> absTohState2;
 TOHState<numDisks-4> absTohState3;
-TOHPDB<numDisks-2, numDisks> pdb1(&absToh1, goal);
-TOHPDB<numDisks-2, numDisks> pdb1a(&absToh1, goal);
-TOHPDB<numDisks-2, numDisks> pdb1b(&absToh1, goal);
-TOHPDB<numDisks-3, numDisks> pdb2(&absToh2, goal);
-TOHPDB<numDisks-4, numDisks> pdb3(&absToh3, goal);
+TOHPDB<numDisks-6, numDisks> pdb1a(&absToh1, goal);
+TOHPDB<numDisks-6, numDisks> pdb1b(&absToh1, goal);
+
+TOHPDB<numDisks-6, numDisks> pdb1(&absToh1, goal);
+TOHPDB<numDisks-12, numDisks, 12> pdb2(&absToh2, goal);
+TOHPDB<numDisks-12, numDisks, 12> pdb3(&absToh2, goal);
+TOHPDB<numDisks-6, numDisks, 6> pdb4(&absToh1, goal);
+
+//TOHPDB<numDisks-4, numDisks> pdb3(&absToh3, goal);
 
 Heuristic<TOHState<numDisks>> h;
 
@@ -98,19 +105,19 @@ void SolveProblem(bool reset = true)
 //	std::cout << "\n";
 //	printf("%1.2fs elapsed; %llu expanded; %llu generated [%lu]\n", t.GetElapsedTime(), ida.GetNodesExpanded(), ida.GetNodesTouched(), solution.size());
 
-	auto *heuristic = BuildPDB<14, 12, 2>(g);
+	auto *heuristic = BuildPDB<numDisks, numDisks-3>(g);
 
 	astar.SetHeuristic(heuristic);
 
-	toh.pruneActions = true;
-	t.StartTimer();
-	astar.GetPath(&toh, s, g, solution);
-	t.EndTimer();
-	for (auto &m : solution)
-	{
-		std::cout << m << " ";
-	}
-	printf("\n[prune] %1.2fs elapsed; %llu expanded; %llu generated [%lu]\n", t.GetElapsedTime(), astar.GetNodesExpanded(), astar.GetNodesTouched(), solution.size());
+//	toh.pruneActions = true;
+//	t.StartTimer();
+//	astar.GetPath(&toh, s, g, solution);
+//	t.EndTimer();
+//	for (auto &m : solution)
+//	{
+//		std::cout << m << " ";
+//	}
+//	printf("\n[prune] %1.2fs elapsed; %llu expanded; %llu generated [%lu]\n", t.GetElapsedTime(), astar.GetNodesExpanded(), astar.GetNodesTouched(), solution.size());
 
 	toh.pruneActions = false;
 	t.StartTimer();
@@ -170,52 +177,55 @@ void MyWindowHandler(unsigned long windowID, tWindowEventType eType)
 		h.lookups.push_back({kLeafNode, 0, 0});
 		s.StandardStart();
 		g.Reset();
-		SolveProblem(true);
+//		SolveProblem(true);
 //		SpeedTest();
 	}
 }
 
 double v = 1;
 uint64_t counter = 0;
-const int animationFrames = 60;
+const int animationFrames = 20;
 void MyFrameHandler(unsigned long windowID, unsigned int viewport, void *)
 {
-	cameraMoveTo(0, -1, -12.5, 0.05);
-	cameraLookAt(0, 0, 0, 0.2);
-	
-	glEnable(GL_LIGHTING);
-	toh.OpenGLDraw();
+//	cameraMoveTo(0, -1, -12.5, 0.05);
+//	cameraLookAt(0, 0, 0, 0.2);
+	auto &display = GetContext(windowID)->display;
+	display.FillRect({-1, -1, 1, 1}, Colors::black);
+	toh.Draw(display);
+
 	if (solution.size() != 0 && running)
 	{
 		toh.GetNextState(s, solution[0], g);
-		toh.OpenGLDraw(s, g, float(counter)/animationFrames);
-		counter = (counter+1)%animationFrames;
-		if (counter == 0)
+		toh.Draw(display, s);
+		SaveSVG(display, 0);
+//		toh.OpenGLDraw(s, g, float(counter)/animationFrames);
+//		counter = (counter+1)%animationFrames;
+//		if (counter == 0)
 		{
 			toh.ApplyAction(s, solution[0]);
 			solution.erase(solution.begin());
-			if (solution.size() == 0)
-				counter = 10;//recording = false;
+//			if (solution.size() == 0)
+//				counter = 10;//recording = false;
 		}
 	}
 	else {
-		if (solution.size() == 0)
-		{
-			counter--;
-			if (counter == 0)
-				recording = false;
-		}
-		toh.OpenGLDraw(s);
+//		if (solution.size() == 0)
+//		{
+//			counter--;
+//			if (counter == 0)
+//				recording = false;
+//		}
+		toh.Draw(display, s);
 	}
 	
 	if (recording && viewport == GetNumPorts(windowID)-1)
 	{
-		static int cnt = 0;
-		char fname[255];
-		sprintf(fname, "/Users/nathanst/Movies/tmp/TOH-%d%d%d%d", (cnt/1000)%10, (cnt/100)%10, (cnt/10)%10, cnt%10);
-		SaveScreenshot(windowID, fname);
-		printf("Saved %s\n", fname);
-		cnt++;
+//		static int cnt = 0;
+//		char fname[255];
+//		sprintf(fname, "/Users/nathanst/Movies/tmp/TOH-%d%d%d%d", (cnt/1000)%10, (cnt/100)%10, (cnt/10)%10, cnt%10);
+//		SaveScreenshot(windowID, fname);
+//		printf("Saved %s\n", fname);
+//		cnt++;
 	}
 	return;
 	
@@ -230,7 +240,7 @@ void MyDisplayHandler(unsigned long windowID, tKeyboardModifier mod, char key)
 {
 	switch (key)
 	{
-		case '|': resetCamera(); break;
+		case '|': //resetCamera(); break;
 		case 'r': recording = !recording; break;
 		case 's':
 		{
@@ -296,18 +306,23 @@ void MyDisplayHandler(unsigned long windowID, tKeyboardModifier mod, char key)
 			goal.Reset();
 			// 268435456 entries (256 MB)
 			// Average: 87.038921 -> 86.762030 -> 86.483987
-			pdb1.BuildPDB(goal, std::thread::hardware_concurrency());
+//			pdb1.BuildPDB(goal, std::thread::hardware_concurrency());
+//			pdb2.BuildPDB(goal, std::thread::hardware_concurrency());
+//			pdb3.BuildPDB(goal, std::thread::hardware_concurrency());
+//			pdb4.BuildPDB(goal, std::thread::hardware_concurrency());
 
-//			pdb1.DivCompress(2, true); // 128 MB
-//			pdb1.DivCompress(2, true); // 64 MB
 
-			h.lookups.resize(0);
-//			h.lookups.push_back({kMaxNode, 1, 2});
-			h.lookups.push_back({kLeafNode, 0, 0});
+//			h.lookups.resize(0);
+//			h.lookups.push_back({kMaxNode, 1, 4});
+//			h.lookups.push_back({kLeafNode, 0, 0});
 //			h.lookups.push_back({kLeafNode, 1, 1});
-			h.heuristics.resize(0);
-//			h.heuristics.push_back(&toh);
-			h.heuristics.push_back(&pdb1);
+//			h.lookups.push_back({kLeafNode, 2, 2});
+//			h.lookups.push_back({kLeafNode, 3, 3});
+//			h.heuristics.resize(0);
+//			h.heuristics.push_back(&pdb1);
+//			h.heuristics.push_back(&pdb2);
+//			h.heuristics.push_back(&pdb3);
+//			h.heuristics.push_back(&pdb4);
 
 			s.StandardStart();
 			SolveProblem(false);
@@ -331,20 +346,32 @@ Heuristic<TOHState<numDisks>> *BuildPDB(const TOHState<numDisks> &goal)
 	
 	TOHPDB<pdb1Disks, numDisks, pdb2Disks> *pdb1 = new TOHPDB<pdb1Disks, numDisks, pdb2Disks>(&absToh1, goal); // top disks
 	TOHPDB<pdb2Disks, numDisks> *pdb2 = new TOHPDB<pdb2Disks, numDisks>(&absToh2, goal); // bottom disks
+
+	TOHPDB<pdb2Disks, numDisks, pdb1Disks> *pdb3 = new TOHPDB<pdb2Disks, numDisks, pdb1Disks>(&absToh2, goal); // top disks
+	TOHPDB<pdb1Disks, numDisks> *pdb4 = new TOHPDB<pdb1Disks, numDisks>(&absToh1, goal); // bottom disks
 	pdb1->BuildPDB(goal, std::thread::hardware_concurrency());
 	pdb2->BuildPDB(goal, std::thread::hardware_concurrency());
-	
+
+	pdb3->BuildPDB(goal, std::thread::hardware_concurrency());
+	pdb4->BuildPDB(goal, std::thread::hardware_concurrency());
+
 	Heuristic<TOHState<numDisks>> *h = new Heuristic<TOHState<numDisks>>;
 	
 	h->lookups.resize(0);
 	
-	h->lookups.push_back({kAddNode, 1, 2});
+	h->lookups.push_back({kMaxNode, 1, 2});
+	h->lookups.push_back({kAddNode, 3, 2});
+	h->lookups.push_back({kAddNode, 5, 2});
 	h->lookups.push_back({kLeafNode, 0, 0});
 	h->lookups.push_back({kLeafNode, 1, 1});
+	h->lookups.push_back({kLeafNode, 2, 2});
+	h->lookups.push_back({kLeafNode, 3, 3});
 	h->heuristics.resize(0);
 	h->heuristics.push_back(pdb1);
 	h->heuristics.push_back(pdb2);
-	
+	h->heuristics.push_back(pdb3);
+	h->heuristics.push_back(pdb4);
+
 	return h;
 }
 
@@ -353,3 +380,16 @@ bool MyClickHandler(unsigned long , int, int, point3d , tButtonType , tMouseEven
 	return false;
 }
 
+
+void SaveSVG(Graphics::Display &d, int port)
+{
+	const std::string baseFileName = "/Users/nathanst/Pictures/hog2/TOH_";
+	static int count = 0;
+	std::string fname;
+	do {
+		fname = baseFileName+std::to_string(count)+".svg";
+		count++;
+	} while (FileExists(fname.c_str()));
+	printf("Save to '%s'\n", fname.c_str());
+	MakeSVG(d, fname.c_str(), 1024, 1024, port);
+}
